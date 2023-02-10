c21a7f7eb8bb582af6dfa19dac9aab2d59c2bace
vinayakequinix <124994122+vinayakequinix@users.noreply.github.com>
2023-02-10 13:52:27 +0530

    updated code for adding two binary numbers

--- BinaryOperations.cpp
+++ BinaryOperations.cpp
@@ -21,6 +21,30 @@ vector<int> int_to_binary(int num)
 
     return ans ;
 }
+// Adding two binary numbers given in vectors
+vector<int> add_binary(vector<int> a, vector<int> b)
+{
+    int n = 32;
+    vector<int> result(n, 0);
+
+    int carry = 0;
+    for(int i=2;i>=0;i--)
+    {
+        if(carry==0)
+        {
+            result[i] = a[i]^b[i];
+            if(result[i]==0 and i!=0) carry = 1;
+        }
+        else
+        {
+            result[i] = a[i]^b[i]^1;
+            if(a[i]==1 and i!=0) carry = 1;
+            else carry = 0;
+        }
+    }
+
+    return result;
+}
 
 int main() {
 
